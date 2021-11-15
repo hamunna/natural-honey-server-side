aad21d154b2b4e55b100888c55d1a459dc921791
hamunna <hamunna@gmail.com>
2021-11-15 13:53:13 +0600

    DELETE Method applied to Products

--- index.js
+++ index.js
@@ -159,6 +159,15 @@ async function run() {
 
 			res.json(result);
 		});
+
+		// DELETE Product (API)
+		app.delete('/products/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const result = await productsCollection.deleteOne(query);
+
+			res.json(result);
+		});
 	}
 	finally {
 		// await client.close();
